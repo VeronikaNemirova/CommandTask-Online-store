ce893be463335a5033209a6cf06344be55d66b92
VeronikaNemirova <nvn92@tut.by>
2023-01-10 00:02:30 +0300

    feat: add view folder for gh-pages

---
+++ view/app/product.d.ts
@@ -0,0 +1,14 @@
+declare const Products: {
+    id: string;
+    name: string;
+    year: string;
+    price: number;
+    amount: number;
+    mainImg: string;
+    imgOne: string;
+    imgTwo: string;
+    description: string;
+    category: string;
+    discount: string;
+}[];
+export default Products;
